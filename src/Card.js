0416586e893154fa619a8a6c9e9c66ba6a2b9add
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2021-07-21 15:38:52 +0100

    Creating a Card Component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt="Robots" src='https://robohash.org/test?200x200'/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>Jane.doe@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
