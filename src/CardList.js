9aa9317ce1f8895a2d625d4915aee618cc496779
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2021-07-30 11:52:40 +0100

    smiplifying the cardlist component

--- src/CardList.js
+++ src/CardList.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    const cardComponenent = robots.map((user, i)=> {
-        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-    })
     return (
         <div>
-            {cardComponenent}
+            {
+                robots.map((user, i)=> {
+                    return(
+                        <Card 
+                            key={i} 
+                            id={robots[i].id} 
+                            name={robots[i].name}
+                            email={robots[i].email} 
+                        />
+                    ); 
+                })
+            }
         </div>
     );
 }
